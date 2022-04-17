b1af63d29cacf20e2c1d91cf16a43dee89d5f16d
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2022-04-17 14:31:13 -0300

    Função de login e erros criadas, funcionamento da Sidebar

--- scripts.js
+++ scripts.js
@@ -1,35 +1,44 @@
-const participantsUrl = "https://mock-api.driven.com.br/api/v6/uol/participants"; 
+const participantsUrl = "https://mock-api.driven.com.br/api/v6/uol/participants";
+const messagesUrl = "https://mock-api.driven.com.br/api/v6/uol/messages";
 let click;
 let nameUser;
-
-
-
-function userNotExists(){
-    document.querySelector(".login-screen").classList.add("hidden");
-}
+let messages = [];
 
 function login (){
     nameUser = document.querySelector(".user-input").value;
-
+    
     let promise = axios.post(participantsUrl, {name: nameUser});
     
+    promise.then(userAllowed);
+    promise.catch(showErrors);
+    
     console.log(nameUser);
-
-    promise.then(userNotExists)
-    //response.data
-
-    promise.catch(error)
-
+    console.log(promise);
 }
 
-function error(error){
-    console.log(error);
-    // arlet de erro
-    // tratar os erros verificando os status 
-    // 400
-    //error.response.status
+function userAllowed(){
+    document.querySelector(".login-screen").classList.add("hidden");
 }
 
-/*if(!nameUser == ''){
-        document.querySelector(".login-screen").classList.add("hidden");  
-    }*/
+function showErrors(erro){ 
+    let errorValue = erro.response.status;
+    console.log("Status code: " + errorValue);
+    
+    if(nameUser === "" && errorValue == 400){
+        alert(`Erro ${errorValue}! Campo em branco, digite seu usuário`);
+    }
+    else if (errorValue == 400){
+        alert(`Erro ${errorValue}! Este usuário já existe, digite seu usuário novamente`);
+    }
+}
+
+function sidebarOn(){
+    click = document.querySelector(".sidebar");
+    if(click !== null){
+        click.classList.remove("hidden");
+    }   
+}
+
+function sidebarOff(){
+    document.querySelector(".sidebar").classList.add("hidden");
+}
