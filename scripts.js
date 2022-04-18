524303b00bddfc9edce0182b35bd990f035c25ad
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2022-04-18 14:23:27 -0300

    Implementado a visão das mensagens

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 const participantsUrl = "https://mock-api.driven.com.br/api/v6/uol/participants";
 const messagesUrl = "https://mock-api.driven.com.br/api/v6/uol/messages";
-let click;
+
 let nameUser;
-let messages = [];
+
+getMessages();
 
 function login (){
     nameUser = document.querySelector(".user-input").value;
@@ -22,14 +23,53 @@ function userAllowed(){
 
 function showErrors(erro){ 
     let errorValue = erro.response.status;
-    console.log("Status code: " + errorValue);
     
-    if(nameUser === "" && errorValue == 400){
-        alert(`Erro ${errorValue}! Campo em branco, digite seu usuário`);
-    }
-    else if (errorValue == 400){
-        alert(`Erro ${errorValue}! Este usuário já existe, digite seu usuário novamente`);
-    }
+    if(nameUser === "" && errorValue == 400) alert(`Erro ${errorValue}! Campo em branco, digite seu usuário`);
+    
+    else if (errorValue == 400) alert(`Erro ${errorValue}! Este usuário já existe, digite seu usuário novamente`);
+}
+
+function getMessages(){
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promise.then(function(response){
+
+        let divMessages = document.querySelector(".container-messages");
+        divMessages.innerHTML = "";
+
+        for(let i=0; i < response.data.length; i++){
+            switch(response.data[i].type){
+                case "status":
+                    divMessages.innerHTML += `
+                    <div class="status-message">
+                        <div class="display-message">
+                            <p><span class="time">(${response.data[i].time})</span>
+                            <span><strong>${response.data[i].from}</strong> ${response.data[i].text}</span></p>
+                        </div>
+                    </div>`;
+                    break;
+
+                case "message":
+                    divMessages.innerHTML += `
+                    <div class="comum-message">
+                        <div class="display-message">
+                            <p><span class="time">(${response.data[i].time})</span>
+                            <span><strong>${response.data[i].from}</strong> para <strong>${response.data[i].to}</strong>: ${response.data[i].text}</span></p>
+                        </div>
+                    </div>`;
+                    break;
+
+                case "private_message":
+                    divMessages.innerHTML += `
+                    <div class="private-message">
+                        <div class="display-message">
+                            <span class="time">(${response.data[i].time})</span>
+                            <span><p><strong>${response.data[i].from}</strong> reservadamente para <strong>${response.data[i].to}</strong>: ${response.data[i].text}</p></span>
+                        </div>
+                    </div>`;
+                    break;
+            }
+        }
+    });
 }
 
 function sidebarOn(){
